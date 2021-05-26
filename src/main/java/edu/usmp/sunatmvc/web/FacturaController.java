63b7d0150e333fca06b18587512df30e0f971e75
Renzo Salinas <renzos.f15@gmail.com>
2021-05-26 15:52:27 -0500

    Delete method

--- src/main/java/edu/usmp/sunatmvc/web/FacturaController.java
+++ src/main/java/edu/usmp/sunatmvc/web/FacturaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,6 +54,12 @@ public class FacturaController {
     @PutMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Factura> update (@RequestBody Factura f){
         create(f);
+        return new ResponseEntity<Factura>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Factura> delete(@PathVariable int id) {
+        facturaRepository.deleteById(id);
         return new ResponseEntity<Factura>(HttpStatus.OK);
     }
     
